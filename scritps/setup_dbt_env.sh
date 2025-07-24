57d6030b7b01d4eb16233267fd71213db87bd100
sharma-asus <gitprojects2025@gmail.com>
2025-07-24 22:17:50 +0530

    modify the files

--- scritps/setup_dbt_env.sh
+++ scritps/setup_dbt_env.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-#exit immediately if a command exits with a non-zero status
+###########exit immediately if a command exits with a non-zero status
 set -e
 
-#fail the script if any command in a pipeline fails
+###########fail the script if any command in a pipeline fails
 set -o pipefail
 
 echo "Starting DBT Setup script"
 
+###############################################
+###############################################
 if ! command -v python &> /dev/null; then
     echo "Python is not installed. Please install Python and try again."
     exit 1
@@ -17,12 +19,14 @@ else
     python -m venv dbt-env
 fi
 
+################################################
+################################################
 if ! command -v pip &> /dev/null; then
     echo "pip is not installed. Please install pip and try again."
     exit 1
 fi
 
-# Check if the virtual environment was created successfully
+###########Check if the virtual environment was created successfully
 if [ ! -d "dbt-env" ]; then
     echo "Failed to create virtual environment."
     exit 1
@@ -31,7 +35,7 @@ else
 
 fi
 
-# Check if the 'bin' directory exists in the virtual environment
+###########Check if the 'bin' directory exists in the virtual environment
 if [ ! -d "dbt-env/bin" ]; then
     echo "Virtual environment 'dbt-env' does not contain a 'bin' directory."
     exit 1
@@ -41,10 +45,13 @@ else
     pip install dbt-snowflake
 fi
 
-# Check if dbt is installed successfully
+###########Check if dbt is installed successfully
 if ! command -v dbt &> /dev/null; then
     echo "DBT installation failed. Please check the installation logs."
     exit 1
 else
     echo "DBT installed successfully. Version: $(dbt --version)"
 fi
+
+##################################################
+##################################################
